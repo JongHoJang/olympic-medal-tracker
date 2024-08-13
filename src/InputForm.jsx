e58cf0bc1c5c1fd8e60f84d5d3a4db40c23adac8
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-08-13 20:12:15 +0900

    컴포넌트 분리

---
+++ src/InputForm.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+const InputForm = ({ countries, setCountries }) => {
+  // input form에 입력하는 내용
+  const [countryInfo, setCountryInfo] = useState({
+    name: "",
+    gold: 0,
+    silver: 0,
+    bronze: 0,
+  });
+
+  // 국가이름 찾기
+  const searchCountry = countries.find(({ name }) => name === countryInfo.name);
+
+  // 인풋
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setCountryInfo({ ...countryInfo, [name]: value, id: new Date().getTime() });
+  };
+
+  // 추가
+  const AddCountry = (e) => {
+    e.preventDefault();
+
+    if (!searchCountry) {
+      setCountries([...countries, countryInfo]);
+    } else {
+      alert("이미 추가된 나라입니다.");
+    }
+    console.log(countries);
+
+    // input 초기화
+    setCountryInfo({
+      name: "",
+      gold: 0,
+      silver: 0,
+      bronze: 0,
+    });
+  };
+
+  // 수정
+  const handleUpdateCountry = (e) => {
+    e.preventDefault();
+    if (searchCountry) {
+      searchCountry.gold = countryInfo.gold;
+      searchCountry.silver = countryInfo.silver;
+      searchCountry.bronze = countryInfo.bronze;
+
+      setCountries([...countries]);
+    } else if (!searchCountry) {
+      alert("해당 국가가 없습니다. 국가를 먼저 추가해주세요");
+    }
+  };
+  return (
+    <>
+      <div className="input-container">
+        <form onSubmit={AddCountry}>
+          <div className="input-item">
+            <label>국가명</label>
+            <input
+              type="text"
+              placeholder="국가 입력"
+              value={countryInfo.name}
+              name="name"
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="input-item">
+            <label>금메달</label>
+            <input
+              type="number"
+              value={countryInfo.gold}
+              name="gold"
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="input-item">
+            <label>은메달</label>
+            <input
+              type="number"
+              value={countryInfo.silver}
+              name="silver"
+              onChange={onInputChange}
+            />
+          </div>
+          <div className="input-item">
+            <label>동메달</label>
+            <input
+              type="number"
+              value={countryInfo.bronze}
+              name="bronze"
+              onChange={onInputChange}
+            />
+          </div>
+
+          <div className="button-container">
+            <button type="submit">국가 추가</button>
+            <button type="button" onClick={handleUpdateCountry}>
+              업데이트
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default InputForm;
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,9 +1,21 @@
 // Table.jsx
 import React from "react";
 
-const Table = ({ countries, onDelete }) => {
+const Table = ({ countries, setCountries }) => {
   // 금메달 수에 따라 오름차순 정렬
   const sortedCountries = [...countries].sort((a, b) => b.gold - a.gold);
+
+  const handleDeleteCountry = (id) => {
+    console.log(id);
+    const filteredCountry = countries.filter((selected) => {
+      if (selected.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setCountries(filteredCountry);
+  };
 
   return (
     <table className="medal-table">
@@ -24,7 +36,9 @@ const Table = ({ countries, onDelete }) => {
             <td>{country.silver}</td>
             <td>{country.bronze}</td>
             <td>
-              <button onClick={() => onDelete(country.id)}>삭제</button>
+              <button onClick={() => handleDeleteCountry(country.id)}>
+                삭제
+              </button>
             </td>
           </tr>
         ))}
